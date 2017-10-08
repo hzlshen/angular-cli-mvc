d2a02d579c9d94965981e5be80a80e844ae77ae1
meanhead <email@example.com>
2017-10-08 23:08:59 +0800

    product

--- myapp/src/app/product/product.component.ts
+++ myapp/src/app/product/product.component.ts
@@ -1,5 +1,4 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit } from "@angular/core";
 @Component({
   selector: 'app-product',
   templateUrl: './product.component.html',
@@ -7,9 +6,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductComponent implements OnInit {
 
-  constructor() { }
+  private products:Array<Product>;
 
+  constructor() { }
+  //组件生命周期钩子
+  //这个方法会在组件实例化后调用一次
   ngOnInit() {
+    this.products=[
+      new Product(1,"第一个商品",1.99,2.5,"这是一个商品Helloo word",["电子产品","硬件设备"]),
+      new Product(2,"第二个商品",8.49,4.5,"这是二个商品Helloo word",["图书","硬件设备"]),
+      new Product(3,"第三个商品",7.09,1.5,"这是三个商品Helloo word",["服装","硬件设备"]),
+      new Product(4,"第四个商品",6.39,2.5,"这是四个商品Helloo word",["线下","硬件设备"]),
+      new Product(5,"第五个商品",5.49,2.5,"这是五个商品Helloo word",["你好","硬件设备"]),
+      new Product(6,"第六个商品",3.69,3.5,"这是六个商品Helloo word",["电子产品","硬件设备"])
+    ]
+  }
+
+}
+
+export class Product{
+
+  constructor(
+    public id:number,
+    public title:string,
+    public price:number,
+    public rating:number,
+    public desc:string,
+    public categories:Array<string>
+  ){
+
   }
 
 }
