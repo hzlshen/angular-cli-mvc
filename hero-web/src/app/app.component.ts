a6d2a4fa7fe9e13d3bc6d1121c5a73d18400ce74
hzlshen <m15277968105@163.com>
2017-10-09 12:17:19 +0800

    hero id

--- hero-web/src/app/app.component.ts
+++ hero-web/src/app/app.component.ts
@@ -1,10 +1,43 @@
 import { Component } from '@angular/core';
 
+export class Hero{
+  id:number;
+  name:string;
+}
+const HEROES:Hero[]=[
+  {id:11,name:'Mr.Nice'},
+  {id:12,name:'Narco'},
+  {id:13,name:'Bombasto'},
+  {id:14,name:'Celeritas'},
+  {id:15,name:'Magneta'},
+  {id:16,name:'RubberMan'},
+  {id:17,name:'Dynama'},
+  {id:18,name:'Dr IQ'},
+  {id:19,name:'Magma'},
+  {id:20,name:'Tornado'},
+];
+
+
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template: `<h1>{{title}}</h1>
+    <h2>{{hero.name}} details</h2>
+    <div>
+      <label for="">id:</label>
+      {{hero.id}}
+    </div>
+    <div>
+    <label for="">name:</label>
+     <input (ngModel)="hero.name" placeholder="name">
+</div>
+`
 })
+
 export class AppComponent {
-  title = 'app';
+  title = 'Tour of Heroes';
+  hero:Hero={
+    id:1,
+    name:"Windstorm"
+  };
+  heroes =HEROES;
 }
