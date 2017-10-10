ce9d9f5b9020d49ae4d24da1a1279cb949a70089
hzlshen <m15277968105@163.com>
2017-10-10 15:59:15 +0800

    component

--- hero-web/src/app/app.component.ts
+++ hero-web/src/app/app.component.ts
@@ -1,9 +1,6 @@
 import { Component } from '@angular/core';
+import { Hero } from  './hero';
 
-export class Hero{
-  id:number;
-  name:string;
-}
 
 const HEROES: Hero[] = [
   { id: 11, name: 'Mr. Nice' },
@@ -31,14 +28,7 @@ const HEROES: Hero[] = [
     <span class="badge">{{hero.id}}</span>{{hero.name}}
 </li>
 </ul>
-<div *ngIf="selectedHero">
-<h2>{{selectedHero.name}}details!</h2>
-<div><label>id:</label>{{selectedHero.id}}</div>
-      <div>
-        <label>name: </label>
-        <input name="hero" [(ngModel)]="selectedHero.name" placeholder="name"/>
-      </div>
-    </div>
+<hero-detail [hero]="selectedHero" ></hero-detail>
   `,
     styles:[`
     .selected{
--- hero-web/src/app/app.module.ts
+++ hero-web/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import {FormsModule} from "@angular/forms";
+import {HeroDetailComponent} from "./hero-detail.component";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeroDetailComponent
   ],
   //第一次报错原因： 没导入FormsModule 需手动导入
   imports: [
