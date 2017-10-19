e699c1ef8ca0c3c4e986228db8dd80933c1c0330
meanhead <m15277968105@163.com>
2017-10-19 21:50:52 +0800

    get

---
+++ router/src/app/app.module.js
@@ -0,0 +1,41 @@
+"use strict";
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+var platform_browser_1 = require('@angular/platform-browser');
+var core_1 = require('@angular/core');
+var app_routing_module_1 = require('./app-routing.module');
+var app_component_1 = require('./app.component');
+var home_component_1 = require('./home/home.component');
+var product_component_1 = require('./product/product.component');
+var code404_component_1 = require('./code404/code404.component');
+var product_desc_component_1 = require('./product-desc/product-desc.component');
+var seller_info_component_1 = require('./seller-info/seller-info.component');
+var AppModule = (function () {
+    function AppModule() {
+    }
+    AppModule = __decorate([
+        core_1.NgModule({
+            declarations: [
+                app_component_1.AppComponent,
+                home_component_1.HomeComponent,
+                product_component_1.ProductComponent,
+                code404_component_1.Code404Component,
+                product_desc_component_1.ProductDescComponent,
+                seller_info_component_1.SellerInfoComponent
+            ],
+            imports: [
+                platform_browser_1.BrowserModule,
+                app_routing_module_1.AppRoutingModule
+            ],
+            providers: [],
+            bootstrap: [app_component_1.AppComponent]
+        })
+    ], AppModule);
+    return AppModule;
+}());
+exports.AppModule = AppModule;
+//# sourceMappingURL=app.module.js.map
