49df9b8c76491954c234443cdd3d1da16ac70c50
meanhead <m15277968105@163.com>
2017-11-01 00:04:18 +0800

    属性绑定和差值表达式是一回事

--- data-binding/src/app/bind/bind.component.js
+++ data-binding/src/app/bind/bind.component.js
@@ -8,11 +8,21 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
 var core_1 = require('@angular/core');
 var BindComponent = (function () {
     function BindComponent() {
+        //属性绑定
+        //差值表达式和属性绑定 是一回事
+        //只要编写代码的时候同一风格就可以了
+        this.imgUrl = "http://placehold.it/400x400";
     }
     BindComponent.prototype.ngOnInit = function () {
     };
     BindComponent.prototype.doOnClick = function (event) {
         console.log(event);
+    };
+    BindComponent.prototype.doOnInput = function (event) {
+        //打印的是他的dom属性
+        console.log(event.target.value);
+        //HTML属性
+        console.log(event.target.getAttribute("value"));
     };
     BindComponent = __decorate([
         core_1.Component({
--- data-binding/src/app/bind/bind.component.ts
+++ data-binding/src/app/bind/bind.component.ts
@@ -7,12 +7,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BindComponent implements OnInit {
 
+  //属性绑定
+  //差值表达式和属性绑定 是一回事
+  //只要编写代码的时候同一风格就可以了
+  imgUrl:string = "http://placehold.it/400x400"
+
   constructor() { }
 
   ngOnInit() {
   }
   doOnClick(event:any){
     console.log(event);
+  }
+  doOnInput(event:any){
+    //打印的是他的dom属性
+    console.log(event.target.value);
+    //HTML属性
+    console.log(event.target.getAttribute("value"));
   }
 
 }
