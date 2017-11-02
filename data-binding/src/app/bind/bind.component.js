385b22b00f36d9c2b154386f386f025b6b52ad2b
hzlshen <m15277968105@163.com>
2017-11-02 23:23:58 +0800

    双向绑定

--- data-binding/src/app/bind/bind.component.js
+++ data-binding/src/app/bind/bind.component.js
@@ -7,36 +7,41 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
 };
 var core_1 = require('@angular/core');
 var BindComponent = (function () {
+    //属性绑定
+    //差值表达式和属性绑定 是一回事
+    //只要编写代码的时候同一风格就可以了
+    // imgUrl:string = "http://placehold.it/400x400"
+    //
+    // isBlog:boolean = false;
+    // isDev:boolean = true;
+    //
+    // divStyle:any ={
+    //   color:'red',
+    //   background:'yellow'
+    // };
+    //
+    // divClass:any = {
+    //   a:true,
+    //   b:false,
+    //   c:false
+    // };
+    // size:number = 2;
     function BindComponent() {
         var _this = this;
-        //属性绑定
-        //差值表达式和属性绑定 是一回事
-        //只要编写代码的时候同一风格就可以了
-        this.imgUrl = "http://placehold.it/400x400";
-        this.isBlog = false;
-        this.isDev = true;
-        this.divStyle = {
-            color: 'red',
-            background: 'yellow'
-        };
-        this.divClass = {
-            a: true,
-            b: false,
-            c: false
-        };
-        this.size = 2;
         setTimeout(function () {
-            _this.isBlog = true;
-            _this.divClass = {
-                a: false,
-                b: true,
-                c: true
-            };
-            _this.isDev = false;
-            _this.divStyle = {
-                color: 'yellow',
-                background: 'red'
-            };
+            // this.isBlog = true;
+            // this.divClass ={
+            //   a:false,
+            //   b:true,
+            //   c:true
+            // };
+            // this.isDev =false;
+            //
+            // this.divStyle ={
+            //   color:'yellow',
+            //   background:'red'
+            // };
+            _this.name = "MMp";
         }, 3000);
     }
     BindComponent.prototype.ngOnInit = function () {
@@ -46,9 +51,10 @@ var BindComponent = (function () {
     };
     BindComponent.prototype.doOnInput = function (event) {
         //打印的是他的dom属性
-        console.log(event.target.value);
+        //console.log(event.target.value);
         //HTML属性
-        console.log(event.target.getAttribute("value"));
+        //console.log(event.target.getAttribute("value"));
+        this.name = event.target.value;
     };
     BindComponent = __decorate([
         core_1.Component({
--- data-binding/src/app/bind/bind.component.ts
+++ data-binding/src/app/bind/bind.component.ts
@@ -10,38 +10,39 @@ export class BindComponent implements OnInit {
   //属性绑定
   //差值表达式和属性绑定 是一回事
   //只要编写代码的时候同一风格就可以了
-  imgUrl:string = "http://placehold.it/400x400"
+  // imgUrl:string = "http://placehold.it/400x400"
+  //
+  // isBlog:boolean = false;
+  // isDev:boolean = true;
+  //
+  // divStyle:any ={
+  //   color:'red',
+  //   background:'yellow'
+  // };
+  //
+  // divClass:any = {
+  //   a:true,
+  //   b:false,
+  //   c:false
+  // };
 
-  isBlog:boolean = false;
-  isDev:boolean = true;
-
-  divStyle:any ={
-    color:'red',
-    background:'yellow'
-  };
-
-  divClass:any = {
-    a:true,
-    b:false,
-    c:false
-  };
-
-  size:number = 2;
+  // size:number = 2;
 
   constructor() {
     setTimeout(()=>{
-      this.isBlog = true;
-      this.divClass ={
-        a:false,
-        b:true,
-        c:true
-      };
-      this.isDev =false;
-
-      this.divStyle ={
-        color:'yellow',
-        background:'red'
-      };
+      // this.isBlog = true;
+      // this.divClass ={
+      //   a:false,
+      //   b:true,
+      //   c:true
+      // };
+      // this.isDev =false;
+      //
+      // this.divStyle ={
+      //   color:'yellow',
+      //   background:'red'
+      // };
+      this.name = "MMp"
     },3000);
 
   }
@@ -51,11 +52,14 @@ export class BindComponent implements OnInit {
   doOnClick(event:any){
     console.log(event);
   }
+
+  name:string;
   doOnInput(event:any){
     //打印的是他的dom属性
-    console.log(event.target.value);
+    //console.log(event.target.value);
     //HTML属性
-    console.log(event.target.getAttribute("value"));
+    //console.log(event.target.getAttribute("value"));
+    this.name = event.target.value;
   }
 
 }
