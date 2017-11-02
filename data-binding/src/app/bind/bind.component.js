3763fa993770007cc196a064114b7bc5b5bb2399
hzlshen <m15277968105@163.com>
2017-11-02 23:39:41 +0800

    响应式编程

--- data-binding/src/app/bind/bind.component.js
+++ data-binding/src/app/bind/bind.component.js
@@ -6,55 +6,15 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
     return c > 3 && r && Object.defineProperty(target, key, r), r;
 };
 var core_1 = require('@angular/core');
+var rxjs_1 = require("rxjs");
 var BindComponent = (function () {
-    //属性绑定
-    //差值表达式和属性绑定 是一回事
-    //只要编写代码的时候同一风格就可以了
-    // imgUrl:string = "http://placehold.it/400x400"
-    //
-    // isBlog:boolean = false;
-    // isDev:boolean = true;
-    //
-    // divStyle:any ={
-    //   color:'red',
-    //   background:'yellow'
-    // };
-    //
-    // divClass:any = {
-    //   a:true,
-    //   b:false,
-    //   c:false
-    // };
-    // size:number = 2;
     function BindComponent() {
-        var _this = this;
-        setTimeout(function () {
-            // this.isBlog = true;
-            // this.divClass ={
-            //   a:false,
-            //   b:true,
-            //   c:true
-            // };
-            // this.isDev =false;
-            //
-            // this.divStyle ={
-            //   color:'yellow',
-            //   background:'red'
-            // };
-            _this.name = "MMp";
-        }, 3000);
+        rxjs_1.Observable.from([1, 2, 3, 4])
+            .filter(function (e) { return e % 2 == 0; })
+            .map(function (e) { return e * e; })
+            .subscribe(function (e) { return console.log(e); }, function (err) { return console.log(err); }, function () { return console.log("结束啦"); });
     }
     BindComponent.prototype.ngOnInit = function () {
-    };
-    BindComponent.prototype.doOnClick = function (event) {
-        console.log(event);
-    };
-    BindComponent.prototype.doOnInput = function (event) {
-        //打印的是他的dom属性
-        //console.log(event.target.value);
-        //HTML属性
-        //console.log(event.target.getAttribute("value"));
-        this.name = event.target.value;
     };
     BindComponent = __decorate([
         core_1.Component({
--- data-binding/src/app/bind/bind.component.ts
+++ data-binding/src/app/bind/bind.component.ts
@@ -1,65 +1,31 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from "rxjs";
 
 @Component({
   selector: 'app-bind',
   templateUrl: './bind.component.html',
   styleUrls: ['./bind.component.css']
 })
+
+//响应式编程就是异步数据流编程
 export class BindComponent implements OnInit {
 
-  //属性绑定
-  //差值表达式和属性绑定 是一回事
-  //只要编写代码的时候同一风格就可以了
-  // imgUrl:string = "http://placehold.it/400x400"
-  //
-  // isBlog:boolean = false;
-  // isDev:boolean = true;
-  //
-  // divStyle:any ={
-  //   color:'red',
-  //   background:'yellow'
-  // };
-  //
-  // divClass:any = {
-  //   a:true,
-  //   b:false,
-  //   c:false
-  // };
 
-  // size:number = 2;
 
   constructor() {
-    setTimeout(()=>{
-      // this.isBlog = true;
-      // this.divClass ={
-      //   a:false,
-      //   b:true,
-      //   c:true
-      // };
-      // this.isDev =false;
-      //
-      // this.divStyle ={
-      //   color:'yellow',
-      //   background:'red'
-      // };
-      this.name = "MMp"
-    },3000);
+    Observable.from([1,2,3,4])
+      .filter(e=>e%2 == 0)
+      .map(e=>e*e)
+      .subscribe(
+        e=>console.log(e),
+      err=>console.log(err),
+        ()=>console.log("结束啦")
+      )
 
   }
 
   ngOnInit() {
   }
-  doOnClick(event:any){
-    console.log(event);
-  }
 
-  name:string;
-  doOnInput(event:any){
-    //打印的是他的dom属性
-    //console.log(event.target.value);
-    //HTML属性
-    //console.log(event.target.getAttribute("value"));
-    this.name = event.target.value;
-  }
 
 }
