f3132e8100f30e34ffcd6a9435d60a931415bb94
hzlshen <m15277968105@163.com>
2017-11-06 21:26:15 +0800

    组建通讯

---
+++ component-commun/src/app/app.module.ts
@@ -0,0 +1,18 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { OrderComponent } from './order/order.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    OrderComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
