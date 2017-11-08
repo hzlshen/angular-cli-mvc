bd62c8c81b85c3dee8be65317ac1c1434816847b
hzlshen <m15277968105@163.com>
2017-11-08 22:12:45 +0800

    oq不分 晚上敲没精神呐

--- component-commun/src/app/price-quotee/price-quotee.component.ts
+++ component-commun/src/app/price-quotee/price-quotee.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, EventEmitter, Qutput } from '@angular/core';
+import { Component, OnInit, EventEmitter,Output } from '@angular/core';
 
 @Component({
   selector: 'app-price-quotee',
@@ -11,7 +11,7 @@ export class PriceQuoteeComponent implements OnInit {
 
   price:number;
 
-  @Qutput()
+  @Output()
   lastPrice:EventEmitter<PriceQuote> = new EventEmitter();
 
   constructor() {
