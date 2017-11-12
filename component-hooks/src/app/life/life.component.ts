9ed334562af06981c029a40d660d78affe994a24
hzlshen <m15277968105@163.com>
2017-11-12 23:06:21 +0800

    声明钩子并在控制台打印钩子的调用顺序

--- component-hooks/src/app/life/life.component.ts
+++ component-hooks/src/app/life/life.component.ts
@@ -1,15 +1,62 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnChanges, DoCheck,AfterContentInit, AfterContentChecked, AfterViewInit, AfterViewChecked ,OnDestroy,
+  SimpleChanges,
+  Input,} from '@angular/core';
+
+let logIndex :number = 1;
 
 @Component({
   selector: 'app-life',
   templateUrl: './life.component.html',
   styleUrls: ['./life.component.css']
 })
-export class LifeComponent implements OnInit {
+//声明所有声明周期钩子
+export class LifeComponent implements OnInit,OnChanges,DoCheck,AfterContentInit,AfterContentChecked,AfterViewInit,AfterViewChecked,OnDestroy {
 
-  constructor() { }
+  //显示钩子调用顺序
+  @Input()
+  name:string;
+
+  logIt(msg:string ){
+    console.log(`#${logIndex} ${msg}`);
+  }
+
+  constructor() {
+    this.logIt("name属性在constructor里的值是："+name);
+  }
+
+  ngOnChanges(changes: SimpleChanges): void {
+
+    let name =  changes['name'].currentValue;
+
+    this.logIt("name属性在ngOnChanges里的值是："+name);
+  }
+
+  ngDoCheck(): void {
+    this.logIt("ngDoCheck");
+  }
+
+  ngAfterContentInit(): void {
+    this.logIt("ngAfterContentInit");
+  }
+
+  ngAfterContentChecked(): void {
+    this.logIt("ngAfterContentChecked");
+  }
+
+  ngAfterViewInit(): void {
+    this.logIt("ngAfterViewInit");
+  }
+
+  ngAfterViewChecked(): void {
+    this.logIt("ngAfterViewChecked");
+  }
+
+  ngOnDestroy(): void {
+    this.logIt("ngOnDestroy");
+  }
 
   ngOnInit() {
+    this.logIt("ngOnInit");
   }
 
 }
