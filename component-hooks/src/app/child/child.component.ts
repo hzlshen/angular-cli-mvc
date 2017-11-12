e0959b30434610dd88a5ef7935977e6dd4f8daab
hzlshen <m15277968105@163.com>
2017-11-13 01:00:31 +0800

    ngDoCheck的实现

--- component-hooks/src/app/child/child.component.ts
+++ component-hooks/src/app/child/child.component.ts
@@ -1,11 +1,11 @@
-import { Component, OnInit,Input, OnChanges, SimpleChanges } from '@angular/core';
+import { Component, OnInit,Input, OnChanges, SimpleChanges,DoCheck } from '@angular/core';
 
 @Component({
   selector: 'app-child',
   templateUrl: './child.component.html',
   styleUrls: ['./child.component.css']
 })
-export class ChildComponent implements OnInit,OnChanges {
+export class ChildComponent implements OnInit,OnChanges,DoCheck {
 
   //注解输入属性
   @Input()
@@ -15,6 +15,29 @@ export class ChildComponent implements OnInit,OnChanges {
   user:{name:string};
 
   message:string = "初始化消息";
+
+  oldUsername:string;
+
+  changesDetected:boolean=false;
+
+  ngChangeCount:number = 0;
+
+  ngDoCheck(): void {
+    if(this.user.name !== this.oldUsername){
+      this.changesDetected = true;
+      console.log("DoCheck:user.name从"+this.oldUsername+"变为"+this.user.name);
+      this.oldUsername = this.user.name;
+    }
+
+    if(this.changesDetected){
+      this.ngChangeCount =   0;
+    }else {
+      this.ngChangeCount =this.ngChangeCount+1;
+      console.log("DoCheck:user.name没变化时ngDoCheck方法已经被调用"+this.ngChangeCount+"次");
+    };
+
+    this.changesDetected = false;
+  }
 
   constructor() { }
 
