2caa52344f120cf5ff2c7debcedcfa2637cc1d41
hzlshen <m15277968105@163.com>
2017-11-21 21:46:36 +0800

    解决app-

--- form-demo/src/app/app.module.ts
+++ form-demo/src/app/app.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import {AppComponent} from "./app.component";
 import { FormsModule,ReactiveFormsModule } from '@angular/forms';
+import {HttpModule} from '@angular/http';
 
 import { TemplateFormComponent } from './template-form/template-form.component';
 import { ReactiveFormComponent } from './reactive-form/reactive-form.component';
@@ -17,6 +18,7 @@ import { ReactiveFormComponent } from './reactive-form/reactive-form.component';
   imports: [
     BrowserModule,
     FormsModule,
+    HttpModule,
     ReactiveFormsModule
   ],
   providers: [],
