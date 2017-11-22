35cd04d4bb3ee46ced89c80f3aca4d19661968c3
hzlshen <m15277968105@163.com>
2017-11-23 00:09:57 +0800

    AbstractControl 校验

--- form-demo/src/app/reactive-regist/reactive-regist.component.ts
+++ form-demo/src/app/reactive-regist/reactive-regist.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import {FormGroup,FormControl,FormBuilder} from '@angular/forms';
+import {FormGroup,FormControl,FormBuilder,AbstractControl,Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-reactive-regist',
@@ -8,12 +8,17 @@ import {FormGroup,FormControl,FormBuilder} from '@angular/forms';
 })
 export class ReactiveRegistComponent implements OnInit {
 
+  //表单校验
+  xxxx(control:AbstractControl):{[key:string]: any}{
+    return null;
+  }
+
   formModel:FormGroup;
 
   //formbuilder重构formGroup表单结构
   constructor(fb:FormBuilder) {
     this.formModel = fb.group({
-      username:[''],
+      username:['',[Validators.required,Validators.minLength(6)]],
       mobile:[''],
       passwordsGroup:fb.group({
         password:[''],
