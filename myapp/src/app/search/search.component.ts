bce7349ce9fcc302a7f5f3715518612eac23bbd8
hzlshen <m15277968105@163.com>
2017-12-03 23:18:57 +0800

    搜索form表单校验

--- myapp/src/app/search/search.component.ts
+++ myapp/src/app/search/search.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {FormGroup,FormControl,FormBuilder,Validators} from '@angular/forms';
+import {ProductService} from "../shared/product.service";
 
 @Component({
   selector: 'app-search',
@@ -7,9 +9,39 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SearchComponent implements OnInit {
 
-  constructor() { }
+  formModel:FormGroup;
+
+  categories: string[];
+
+  constructor(private productService:ProductService) {
+    let fb = new FormBuilder();
+    this.formModel = fb.group({
+      title:['',Validators.minLength(3)],
+      price:[null,this.positiveNumberValidator],
+      category:['-1']
+    });
+  }
 
   ngOnInit() {
+    this.categories = this.productService.getAllCategories();
+  }
+
+  positiveNumberValidator(control:FormControl):any{
+    if(!control.value){
+      return null;
+    }
+    let price = parseInt(control.value);
+
+    if(price>0){
+      return null;
+    }else{
+      return {positiveNumber:true};
+    }
+  }
+  onSearch(){
+    if(this.formModel.valid){
+      console.log(this.formModel.value);
+    }
   }
 
 }
