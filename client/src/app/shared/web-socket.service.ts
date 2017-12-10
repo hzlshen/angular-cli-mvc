ea1b6d0e14e099c61aa66757367d5af3b07f61ba
hzlshen <m15277968105@163.com>
2017-12-10 22:25:51 +0800

    服务ok

--- client/src/app/shared/web-socket.service.ts
+++ client/src/app/shared/web-socket.service.ts
@@ -1,8 +1,28 @@
 import { Injectable } from '@angular/core';
+import {Observable} from "rxjs";
+
 
 @Injectable()
 export class WebSocketService {
 
+  ws:WebSocket;
+
   constructor() { }
+
+  createObservableSocket(url:string):Observable<any>{
+    this.ws = new WebSocket(url);
+    // 返回一个可观测的流
+    return new Observable(
+      observable=>{
+        this.ws.onmessage = (event)=>observable.next(event.data);
+        this.ws.onerror = (event)=>observable.error(event);
+        this.ws.onclose = (event)=>observable.complete();
+      }
+    );
+  }
+
+  sendMessage(message:string){
+    this.ws.send(message);
+  }
 
 }
