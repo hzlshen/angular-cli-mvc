946ee75f705eec0d9724ec009dd962ff39c21d6f
hzlshen <m15277968105@163.com>
2017-12-10 22:43:45 +0800

    编写websocket组件 使用流

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -6,6 +6,7 @@ import { ProductComponent } from './product/product.component';
 import {FormsModule} from "@angular/forms";
 import {HttpModule} from "@angular/http";
 import { WebSocketComponent } from './web-socket/web-socket.component';
+import {WebSocketService} from "./shared/web-socket.service";
 
 @NgModule({
   declarations: [
@@ -18,7 +19,9 @@ import { WebSocketComponent } from './web-socket/web-socket.component';
     HttpModule,
     FormsModule
   ],
-  providers: [],
+  providers: [
+    WebSocketService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- client/src/app/web-socket/web-socket.component.ts
+++ client/src/app/web-socket/web-socket.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {WebSocketService} from "../shared/web-socket.service";
 
 @Component({
   selector: 'app-web-socket',
@@ -7,9 +8,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class WebSocketComponent implements OnInit {
 
-  constructor() { }
+  // 通过依赖注入 注入WebSocketService
+  constructor(private wsService:WebSocketService) { }
 
   ngOnInit() {
+    this.wsService.createObservableSocket("ws://localhost:8085")
+      .subscribe(
+        data =>console.log(data),
+        err=>console.log(err),
+        ()=>console.log("流已经结束")
+      );
+  }
+
+  sendMessageToServer(){
+    this.wsService.sendMessage("hello form client");
   }
 
 }
