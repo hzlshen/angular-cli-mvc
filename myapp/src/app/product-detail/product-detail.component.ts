08fdcbd8e8889f192e9f2b7cff2a8a6df7d110db
hzlshen <m15277968105@163.com>
2017-12-12 00:13:35 +0800

    增加proxy config配置端口文件

--- myapp/src/app/product-detail/product-detail.component.ts
+++ myapp/src/app/product-detail/product-detail.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit} from '@angular/core';
 import { ActivatedRoute} from '@angular/router';
-import {Product, ProductService,Comment} from "../shared/product.service";
+import {Product, ProductService} from "../shared/product.service";
 
 @Component({
   selector: 'app-product-detail',
